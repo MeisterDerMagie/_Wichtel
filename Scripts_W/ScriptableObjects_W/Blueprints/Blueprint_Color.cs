28bca851e4fe9bdc43ddef18e07852196ffbe1e1
Martin <martin.kloeckener@takomat.com>
2019-04-12 16:20:50 +0200

    added UI_SetColorOfGraphicElement and UI_SetColorOfSpriteElement

---
+++ Scripts_W/ScriptableObjects_W/Blueprints/Blueprint_Color.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wichtel.UI{    
+    [CreateAssetMenu(fileName = "scrOb_Color", menuName = "Scriptable Objects/Color", order = 0)]
+public class Blueprint_Color : ScriptableObject {
+
+    public Color color;
+}
+}
