387a7e11a0844d02b0201aee841130e7a141d5b5
Martin_Home <martin.kloeckener@gmail.com>
2019-12-29 23:35:12 +0100

    ColorExtensions

---
+++ Scripts_W/Extensions_W/ColorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wichtel.Extensions{
+public static class ColorExtensions
+{
+    //use this to change only one, two or three values of a Color
+    public static Color With(this Color original, float? r = null, float? g = null, float? b = null, float? a = null)
+    {
+        float newR = r ?? original.r;
+        float newG = g ?? original.g;
+        float newB = b ?? original.b;
+        float newA = a ?? original.a;
+        
+        return new Color(newR, newG, newB, newA);
+    }
+}
+}
