8c5cf486e62ce98b4d717709342ce70926f14894
Martin_Home <martin.kloeckener@gmail.com>
2020-04-03 16:34:52 +0200

    StringExtension: Substring between two strings

---
+++ Scripts_W/Extensions_W/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Wichtel.Extensions{
+public static class StringExtensions
+{
+    public static string Substring(this string stringToGetSubstringFrom, string beginAfter, string endAt)
+    {
+        int pFrom = stringToGetSubstringFrom.IndexOf(beginAfter) + beginAfter.Length;
+        int pTo = stringToGetSubstringFrom.LastIndexOf(endAt);
+
+        return stringToGetSubstringFrom.Substring(pFrom, pTo - pFrom);
+    }
+}
+}
