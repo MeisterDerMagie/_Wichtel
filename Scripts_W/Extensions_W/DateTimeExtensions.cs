db2711ef7488a577a30f38a227f52b8e47e137bd
Martin_Home <martin.kloeckener@gmail.com>
2020-05-03 19:34:47 +0200

    DateTimeExtensions

---
+++ Scripts_W/Extensions_W/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wichtel.Extensions
+{
+public static class DateTimeExtensions
+{
+    public static TimeSpan GetDuration(DateTime _olderDate, DateTime _youngerDate)
+    {
+        return _youngerDate.Subtract(_olderDate);
+    }
+}
+}
