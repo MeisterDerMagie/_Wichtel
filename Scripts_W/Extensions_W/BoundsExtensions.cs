3b4b3cd3e41a52de7b823531915aa14bbb28a2d6
Martin_Home <martin.kloeckener@gmail.com>
2020-05-06 22:23:23 +0200

    BoundsExtensions

---
+++ Scripts_W/Extensions_W/BoundsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Wichtel.Extensions {
+public static class BoundsExtensions
+{
+    public static Bounds CalculateBoundsInChildren(Transform _transform)
+    {
+        Bounds bounds = new Bounds(_transform.position, Vector3.zero);
+ 
+        foreach(Renderer renderer in _transform.GetComponentsInChildren<Renderer>())
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        return bounds;
+    }
+}
+}
