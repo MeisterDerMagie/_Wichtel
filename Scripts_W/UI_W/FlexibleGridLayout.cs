fbbeb2ec079d67030cf49c7749437d45db596099
Martin_Home <martin.kloeckener@gmail.com>
2020-05-07 21:38:35 +0200

    FlexibleGridLayout (wip)

---
+++ Scripts_W/UI_W/FlexibleGridLayout.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wichtel.UI{
+public class FlexibleGridLayout : LayoutGroup
+{
+    public enum FitType
+    {
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColumns
+    }
+    
+    public Vector2 spacing;
+    public FitType fitType;
+    [Min(1)][EnableIf("fitTypeIsFixedRows")]    public int rows;
+    [Min(1)][EnableIf("fitTypeIsFixedColumns")] public int columns;
+    [EnableIf("notFitXOrY")]                    public Vector2 cellSize;
+    [EnableIf("fixedRowsOrFixedColums")]        public bool fitX;
+    [EnableIf("fixedRowsOrFixedColums")]        public bool fitY;
+    
+    public override void CalculateLayoutInputHorizontal()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        if (fitType == FitType.Width || fitType == FitType.Height || fitType == FitType.Uniform)
+        {
+            fitX = true;
+            fitY = true;
+            
+            float sqrRt = Mathf.Sqrt(transform.childCount);
+            rows = Mathf.CeilToInt(sqrRt);
+            columns = Mathf.CeilToInt(sqrRt);
+        }
+
+        if (fitType == FitType.Width || fitType == FitType.FixedColumns)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+        else if (fitType == FitType.Height || fitType == FitType.FixedRows)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
+        
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)(columns)) * 2) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)(rows)) * 2) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = fitX ? cellWidth : cellSize.x;
+        cellSize.y = fitY ? cellHeight : cellSize.y;
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            int rowCount = i / columns;
+            int columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+            
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+    }
+    
+    //--- für Odin ---
+    #region Odin
+    private bool fitTypeIsFixedRows => (fitType == FitType.FixedRows);
+    private bool fitTypeIsFixedColumns => (fitType == FitType.FixedColumns);
+    private bool notFitXOrY => (!fitX || !fitY);
+    private bool fixedRowsOrFixedColums => (fitType == FitType.FixedRows || fitType == FitType.FixedColumns);
+    #endregion
+    //--- ---
+
+    public override void CalculateLayoutInputVertical()
+    {
+        
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+        
+    }
+
+    public override void SetLayoutVertical()
+    {
+        
+    }
+}
+}
