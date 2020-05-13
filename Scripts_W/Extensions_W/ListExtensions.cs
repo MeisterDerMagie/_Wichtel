a4cce075beb18508b5008476f726ca0480c81fc0
Martin_Home <martin.kloeckener@gmail.com>
2020-05-13 20:25:37 +0200

    ListExtensions

--- Scripts_W/Extensions_W/ListExtensions.cs
+++ Scripts_W/Extensions_W/ListExtensions.cs
@@ -5,10 +5,7 @@ public static class ListExtensions
 {
     private static void RemoveEmptyEntries<T>(this List<T> _list) where T : class
     {
-        for (int i = _list.Count-1; i >= 0; i--)
-        {
-            if(_list[i] == null) _list.RemoveAt(i);
-        }
+        _list.RemoveAll(item => item == null);
     }
 }
 }
