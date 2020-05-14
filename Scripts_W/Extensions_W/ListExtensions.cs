6b698f18399e2b773b7c42b8a62bfd10cc1ca109
Martin_Home <martin.kloeckener@gmail.com>
2020-05-14 23:30:53 +0200

    ListExtensions

--- Scripts_W/Extensions_W/ListExtensions.cs
+++ Scripts_W/Extensions_W/ListExtensions.cs
@@ -3,7 +3,7 @@
 namespace Wichtel.Extensions{
 public static class ListExtensions
 {
-    private static void RemoveEmptyEntries<T>(this List<T> _list) where T : class
+    public static void RemoveEmptyEntries<T>(this List<T> _list) where T : class
     {
         _list.RemoveAll(item => item == null);
     }
